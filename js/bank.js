5e127f6298f199500a0c371a80f65d057d8a19fa
caden-knight <cadenknight@gmail.com>
2021-05-09 15:47:24 -0600

    initial commit

---
+++ js/bank.js
@@ -0,0 +1,34 @@
+// Keeping track of money
+let bank = {
+    wallet: 5000,
+    bankAcct: 0,
+    offshore: 0,
+    businessEarnings: {
+        counterfeit: 0,
+        nightclub: 0,
+        drugLord: 0,
+        
+    }
+}
+
+let items = {
+    counterfeit: {
+        printer: 2500
+    }
+}
+
+let employees = {
+    counterfeit: {
+        production: {
+            printOperater: 1,
+        },
+        export: {
+            moneyRunner: 1,
+
+        }
+    }
+}
+
+let wallet = `$${bank.wallet}`
+let bank = `$${bank}`
+let offshore = `$${offshore}`
---
+++ js/index.js
@@ -0,0 +1,17 @@
+let data = {
+    money: 1,
+    multiplier: 1
+}
+
+let money = document.getElementById("moneyAmt")
+let startBtn = document.getElementById("start")
+let drinkBtn = document.getElementById("drink")
+
+
+function moneyCounter() {
+    
+}
+
+function buyBusiness() {
+    
+}
